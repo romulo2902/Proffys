52c17a58640f9e6f3b630ffc028b9fb796153c52
Romulo Valadares <romulo2902@gmail.com>
2020-08-17 16:10:49 -0300

    criando estilo para index

--- web/web/src/App.tsx
+++ web/web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Landing from './pages/landing';
+
+import './assets/styles/global.css';
+
 
 function App() {
   return (
-    <div className="App">
- <h1>Hello World</h1>
-    </div>
+    <Landing />
   );
 }
 
